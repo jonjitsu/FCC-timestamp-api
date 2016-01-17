f5375ab06ad085be44224cae3b4f65c6937be6ec
Jonathan Willis <jonathanwillis@linux.com>
2016-01-17 10:35:41 -0500

    Build for heroku

--- build/server.js
+++ build/server.js
@@ -5,9 +5,12 @@ const PORT = process.env.PORT || 3000;
 var assert = require('assert'),
     express = require('express'),
     engines = require('consolidate'),
+    ts = require('./timestamps.js'),
     app = express().engine('html', engines.nunjucks).set('view engine', 'html').set('views', __dirname + '/views').get('/', function (req, res) {
     // res.send('It Works JW!');
     res.render('welcome');
+}).get('/:value', function (req, res) {
+    res.json(ts.response(ts.parseValue(req.params.value)));
 }).use(function (req, res) {
     res.sendStatus(404);
 }),

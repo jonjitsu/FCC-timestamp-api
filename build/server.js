69ea4628536322eee7ee4ac82ff83d7a50028dd9
Jonathan Willis <jonathanwillis@linux.com>
2016-01-16 23:11:50 -0500

    Added babel

---
+++ build/server.js
@@ -0,0 +1,13 @@
+var assert = require('assert'),
+    PORT = process.env.PORT || 3000,
+    express = require('express'),
+    engines = require('consolidate'),
+    app = express().engine('html', engines.nunjucks).set('view engine', 'html').set('views', __dirname + '/views').get('/', function (req, res) {
+    res.send('It Works JW!');
+}).use(function (req, res) {
+    res.sendStatus(404);
+}),
+    server = app.listen(PORT, function () {
+    var port = server.address().port;
+    console.log('Up an runnin');
+});
